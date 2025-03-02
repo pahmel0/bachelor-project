8b5f5cf992cb5b629947319f824cc183c2b1532e
pahmel0 <mantasx5@gmail.com>
2025-03-02 03:15:22 +0100

    Created a backend structure using Spring Boot initializer

---
+++ backend/src/main/java/com/attvin/waste_management/WasteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.attvin.waste_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/attvin/waste_management/WasteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attvin.waste_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
