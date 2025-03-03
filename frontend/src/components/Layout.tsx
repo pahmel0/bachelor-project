76e884e0d114c40a2043abec0c4071cb4afdf59e
pahmel0 <mantasx5@gmail.com>
2025-03-03 15:05:46 +0100

    Add Layout component to structure frontend application

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,33 @@
+import { Box, CssBaseline } from "@mui/material";
+import { ReactNode } from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Box sx={{ display: "flex", minHeight: "100vh" }}>
+      <CssBaseline />
+      <Header />
+      <Sidebar />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - 240px)` },
+          ml: { sm: "240px" },
+          mt: "64px",
+          backgroundColor: "background.default",
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
