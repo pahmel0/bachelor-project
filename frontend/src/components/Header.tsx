4fe62d8cf3f861a6d5fc9bc55fb62f940c1493e7
pahmel0 <mantasx5@gmail.com>
2025-04-22 15:02:40 +0200

    Removed search bar from the header and all components associated

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,11 +4,8 @@ import {
   Badge,
   Box,
   IconButton,
-  InputBase,
   Toolbar,
   Typography,
-  alpha,
-  styled,
   Menu,
   MenuItem,
   Divider,
@@ -16,7 +13,6 @@ import {
 } from "@mui/material";
 import {
   Menu as MenuIcon,
-  Search as SearchIcon,
   Notifications as NotificationsIcon,
   Logout as LogoutIcon,
   Settings as SettingsIcon,
@@ -24,45 +20,6 @@ import {
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import authService from "../services/authService";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 const Header = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -116,15 +73,6 @@ const Header = () => {
         <Typography variant="h6" noWrap component="div">
           Material Management System
         </Typography>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-            placeholder="Search materials..."
-            inputProps={{ "aria-label": "search" }}
-          />
-        </Search>
         <Box sx={{ flexGrow: 1 }} />
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <IconButton size="large" color="inherit">
